a083000e45fd69d539b056d0735055581fa3d898
Isaiah <isaiah.g.callano@gmail.com>
2024-03-25 20:37:23 -0700

    Implements token check for adding listing to favorites

---
+++ backend/middleware/check-auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    // skip browser OPTIONS interaction
+    if (req.method === "OPTIONS") {
+        return next();
+    }
+
+    try {
+        const token = req.headers.authorization.split(" ")[1]; // Authorization: "Bearer TOKEN"
+        if (!token) {
+            return next(new Error("Token problem. No token :L"));
+        }
+        const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+        // dynamically adding data to reqest object
+        req.userData = { userId: decodedToken.userId };
+        next();
+    } catch (err) {
+        return next(new Error("Authentication failed. Token problem."));
+    }
+};
