7fd8cdbfe969b3954a8c8791df15a6e7ebd8f8b2
Isaiah <isaiah.g.callano@gmail.com>
2024-03-26 11:42:25 -0700

    Fix express validators on signup and add message on fail

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -8,18 +8,31 @@ const router = express.Router();
 
 router.post(
     "/signup",
-    // [
-    //     check("username").isLength({ min: 4 }),
-    //     check("password").isLength({ min: 8 }),
-    //     check("password").matches(/\d/),
-    //     check("confirmPassword").matches("password")
-    // ],
+    [
+        check("username")
+            .isLength({ min: 4 })
+            .withMessage("Username must be at least 4 characters long"),
+        check("password")
+            .isLength({ min: 8 })
+            .withMessage("Password must be at least 8 characters long"),
+        check("password")
+            .matches(/\d/)
+            .withMessage("Password must contain a number"),
+        check("confirmPassword")
+            .custom((value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error("Passwords do not match");
+                }
+                return true;
+            })
+            .withMessage("Passwords must match"),
+    ],
     userController.signup
 );
 
 router.post("/login", userController.login);
 
-// User needs to be authenticated to change credentials
+// User needs to be authenticated to change credentials, disable when using POSTMAN
 // router.use(checkAuth);
 
 router.patch("/change-credentials", userController.changeCredentials);
