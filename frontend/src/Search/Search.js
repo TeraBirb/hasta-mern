169dbb9ce53fc1fae8f2a65c44e17955d313b5f9
Isaiah <isaiah.g.callano@gmail.com>
2024-03-27 19:29:50 -0700

    Add filter functionality to search

--- frontend/src/Search/Search.js
+++ frontend/src/Search/Search.js
@@ -1,21 +1,24 @@
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import "./Search.css";
 
-const options = {
+let options = {
     method: "GET",
     url: "https://us-real-estate.p.rapidapi.com/v2/for-rent",
     params: {
         city: "San Francisco",
         state_code: "CA",
         limit: "42",
-        offset: "0",
+        beds_min: "1",
+        baths_min: "1",
         price_min: "1000",
         price_max: "5000",
         property_type:
             "apartment, single_family, condo, townhouse, multi_family, land, mobile, other",
         expand_search_radius: "25",
+        //  1|5|10|25|50
     },
     headers: {
         "X-RapidAPI-Key": "e357b96598mshfde163274ffcabep1f1e31jsn8e13ff618822",
@@ -27,28 +30,35 @@ const options = {
 const Search = () => {
     const navigate = useNavigate();
 
-    // const handleSearch = async () => {
-    //     console.log("Searching...");
-    //     // directs to /results
-    //     try {
-    //         const response = await axios.get(process.env.REACT_APP_BACKEND_URL + "/listing/search/TEST");
-    //         // extract data
-    //         const data = response.data;
-
-    //         if (response.status !== 200) {
-    //             throw new Error(data);
-    //         }
-    //         console.log(data);
-    //         // attach data as state to navigate
-    //         navigate("/results", { state: { data: data } });
-    //         window.scrollTo(0, 0);
-    //     } catch (err) {
-    //         console.log(err);
-    //     }
-    // };
+    const [city, setCity] = useState("San Francisco");
+    const [stateCode, setStateCode] = useState("CA");
+    const [minPrice, setMinPrice] = useState("");
+    const [maxPrice, setMaxPrice] = useState("");
+    const [minBedrooms, setMinBedrooms] = useState("1");
+    const [minBathrooms, setMinBathrooms] = useState("1");
+    const [propertyTypes, setPropertyTypes] = useState({
+        any: false,
+        townhome: false,
+        coop: false,
+        condo: false,
+        apartment: false,
+        single_family: false,
+    });
 
     const handleSearch = async () => {
         console.log("Searching...");
+        options.params.city = city;
+        options.params.state_code = stateCode;
+        options.params.price_min = minPrice;
+        options.params.price_max = maxPrice;
+        options.params.beds_min = minBedrooms;
+        options.params.baths_min = minBathrooms;
+
+        const selectedPropertyTypes = Object.entries(propertyTypes)
+            .filter(([key, value]) => value === true)
+            .map(([key]) => key)
+            .join(", ");
+        options.params.property_type = selectedPropertyTypes;
         // directs to /results
         // US REAL ESTATE API
         try {
@@ -83,11 +93,34 @@ const Search = () => {
             } catch (err) {
                 console.log(err);
             }
-
-
         } catch (error) {
             console.error(error);
         }
+    };
+
+    // const handleSearch = () => {
+    //     options.params.city = city;
+    //     options.params.state_code = stateCode;
+    //     options.params.price_min = minPrice;
+    //     options.params.price_max = maxPrice;
+    //     options.params.beds_min = minBedrooms;
+    //     options.params.baths_min = minBathrooms;
+
+    //     const selectedPropertyTypes = Object.entries(propertyTypes)
+    //         .filter(([key, value]) => value === true)
+    //         .map(([key]) => key)
+    //         .join(", ");
+    //     options.params.property_type = selectedPropertyTypes;
+
+    //     console.log(options.params);
+    // };
+
+    const handleCheckboxChange = (event) => {
+        const { name, checked } = event.target;
+        setPropertyTypes({
+            ...propertyTypes,
+            [name]: checked,
+        });
     };
 
     return (
@@ -105,25 +138,59 @@ const Search = () => {
             <div className="filters">
                 <div className="filter-item">
                     <label htmlFor="">Minimum price:</label>
-                    <input type="text" placeholder="$" />
+                    <input
+                        type="text"
+                        placeholder="$"
+                        onInput={(e) => setMinPrice(e.target.value)}
+                    />
                 </div>
                 <div className="filter-item">
                     <label htmlFor="">Maximum price:</label>
-                    <input type="text" placeholder="$" />
+                    <input
+                        type="text"
+                        placeholder="$"
+                        onInput={(e) => setMaxPrice(e.target.value)}
+                    />
                 </div>
                 <div className="filter-item">
                     <label htmlFor="">Bedrooms:</label>
-                    <select name="" id="">
-                        <option value="">any</option>
-                        <option value="">op1</option>
-                        <option value="">op2</option>
+                    <select
+                        value={minBedrooms}
+                        onChange={(e) => setMinBedrooms(e.target.value)}
+                    >
+                        <option value="any">any</option>
+                        <option value="1">1+</option>
+                        <option value="2">2+</option>
+                        <option value="3">3+</option>
+                        <option value="4">4+</option>
                     </select>
                     <label htmlFor="">Bathrooms:</label>
-                    <select name="" id="">
-                        <option value="">any</option>
-                        <option value="">op1</option>
-                        <option value="">op2</option>
+                    <select
+                        value={minBathrooms}
+                        onChange={(e) => setMinBathrooms(e.target.value)}
+                    >
+                        <option value="any">any</option>
+                        <option value="1">1+</option>
+                        <option value="2">2+</option>
+                        <option value="3">3+</option>
+                        <option value="4">4+</option>
                     </select>
+                </div>
+                <div className="filter-item">
+                    <label htmlFor="">Property Type:</label>
+                    <div>
+                        {Object.entries(propertyTypes).map(([key, value]) => (
+                            <label key={key}>
+                                <input
+                                    type="checkbox"
+                                    name={key}
+                                    checked={value}
+                                    onChange={handleCheckboxChange}
+                                />
+                                {key.replace("_", " ")}
+                            </label>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
